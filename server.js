faff6f6f9651d6fe6d4e83b88253cd113ac5de7d
roshniv <heyitsroshni@gmail.com>
2016-11-27 23:48:42 +1100

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,6 @@
 
 var http = require('http');
 var app = require('./app')();
-require('./mocks/user/user-api')(app);
-require('./mocks/survey/survey-api')(app);
-require('./mocks/dashboard/dashboard-api')(app);
-
 var port = process.env.PORT || 3000; 		// set our port
 
 http.createServer(app).listen(port, function () {
